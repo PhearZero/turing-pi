94759a94738463092a55933247575991708837e7
Michael Feher <git@phearzero.com>
2023-04-28 16:54:33 -0500

    🔧 Mound sdcard to opt

--- br2t113pro/board/100ask/rootfs_overlay/etc/init.d/S99hello.sh
+++ br2t113pro/board/100ask/rootfs_overlay/etc/init.d/S99hello.sh
@@ -5,7 +5,10 @@ echo "|_   _| | | |  _ \|_ _| \ | |/ ___|"
 echo "  | | | | | | |_) || ||  \| | |  _ "
 echo "  | | | |_| |  _ < | || |\  | |_| |"
 echo "  |_|  \___/|_| \_\___|_| \_|\____|"
+
+mkdir -p /mnt/sdcard
 mount /dev/mmcblk0p1 /mnt/sdcard/
+mount /dev/mmcblk0p1 /opt/
 echo 3 4 1 7 > /proc/sys/kernel/printk
 
 sleep 1 # wait 
